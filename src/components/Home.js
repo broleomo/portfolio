665f224e3be007f46c3895eadc6b884e3a774c40
broleomo <brooklynnmoor@gmail.com>
2018-09-13 21:03:11 -0400

    project-additions

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,11 +6,11 @@ export default class Input extends Component {
         <div>
         <div style={{display: 'flex', alignItems: 'center', flexDirection:"column"}}>
       <h1 className="heading">Hello, I'm Brooklynn!</h1>
-      <img className="profile-image"src='BrooklynnProfile.jpg' alt="profile"/>
+      <img className="profile-image"src='Oval-profile.png' alt="profile"/>
       </div>
       <div className="line"/>
       <h2>SKILLS</h2>
-      <ul style={{display:'flex', justifyContent: 'center', listStyleType: 'none'}}>
+      <ul style={{display:'flex', justifyContent: 'center', listStyleType: 'disc', listStylePosition: 'inside' }}>
           <li>HTML</li>
           <li>CSS</li>
           <li>Javascript</li>
@@ -25,7 +25,44 @@ export default class Input extends Component {
       </ul>
       <div className="line"/>
       <h2>PROJECTS</h2>
+      <div className="projects-container">
+      <div class="container">
+  <h3 class="title">Music Search Site</h3>
+  <a target="_blank" rel="noopener noreferrer" href="https://broleomo.github.io/Music-Search/">
+  <div class="content">
+      <div class="content-overlay"></div>
+      <img style={{width: '100%'}} class="content-image" alt="music-search" src="vibequest.png"/>
+      <div class="content-details fadeIn-top">
+        <h3>Simple music search app </h3>
+        <ul className="truck-overlay-text"style={{listStyleType: 'none', color: 'white'}}>
+          <li>Javascript</li>
+          <li>iTunes API</li>
+          <li>CSS</li>
+        </ul>
+      </div>
+  </div>
+  </a>
+</div>
 
+      <div class="container">
+  <h3 class="title">Truck Scheduling App</h3>
+  <a target="_blank" rel="noopener noreferrer" href="http://18.219.80.82/">
+  <div class="content">
+      <div class="content-overlay"></div>
+      <img class="content-image" alt="trucking-site" src="oil-recycling.png"/>
+      <div class="content-details fadeIn-top">
+        <h3>Worked on a small team using:</h3>
+        <ul className="truck-overlay-text"style={{listStyleType: 'none', color: 'white'}}>
+          <li>React + Redux</li>
+          <li>Node.js + Express</li>
+          <li>MongoDB + Mongoose</li>
+          <li>CircleCI</li>
+        </ul>
+      </div>
+  </div>
+  </a>
+</div>
+</div>
       </div>
     )
   }
