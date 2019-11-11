c35cefb0a4b9e5a264d0c181ba2e837cc3368e99
Brooklynn Moor <brooklynn.moor@mediacurrent.com>
2019-11-10 17:01:50 -0800

    updated a couple projects and content

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,11 +11,8 @@ export default class Input extends Component {
         <div className="heading-container">
       <h1 className="heading">Hello, I'm Brooklynn!</h1>
       <div className="bio">   
-      <a target="_blank" rel="noopener noreferrer" href="https://www.behance.net/secludedenigma">
       <img className="profile-image"src='Brooklynn-illustrated.png' alt="profile"/>  
-      </a> 
-        <p>I'm a developer currently based in Indianapolis focused on the front end with some back end experience. I am energized by the collaboration of a team and am constantly learning from my peers. 
-          I love connecting with my community and do my best to focus on making positive world changes in everything that I do, professionally and personally.
+        <p>I'm a software engineer based in Portland focused on building beautiful, well-documented, maintainable user experiences. I am energized by the collaboration of a team and am constantly learning and sharing with my peers. 
           <br/>
           <br/>
           I am also the proud mother of a black cat named Salazar who is sleeping above. Click him to see a photo.
@@ -27,34 +24,33 @@ export default class Input extends Component {
       <div className="skills-container">
       <h2>SKILLS</h2>
       <ul className="skills" style={{display:'flex',flexWrap:'wrap', listStyleType: 'disc', listStylePosition: 'inside' }}>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>Javascript</li>
           <li>React</li>
           <li>Redux</li>
           <li>Node.js</li>
+          <li>Gatsby.js</li>
+          <li>Javascript</li>
           <li>Docker</li>
           <li>Sketch</li>
           <li>InVision</li>
-          <li>Wireframing</li>
+          <li>UX/UI</li>
       </ul>
       </div>
       <div className="line"/>
       <h2>PROJECTS</h2>
       <div className="projects-container">
       <div class="container">
-  <h3 class="title">Music Search Site</h3>
-  <a target="_blank" rel="noopener noreferrer" href="https://broleomo.github.io/Music-Search/">
-  <div class="content">
-      <div class="content-overlay"></div>
-      <img style={{width: '100%'}} class="content-image" alt="music-search" src="vibequest.png"/>
-      <div class="content-details fadeIn-top">
+        <h3 class="title">Music Search Site</h3>
+        <a target="_blank" rel="noopener noreferrer" href="https://broleomo.github.io/Music-Search/">
+        <div class="content">
+          <div class="content-overlay"></div>
+            <img style={{width: '100%'}} class="content-image" alt="music-search" src="vibequest.png"/>
+            <div class="content-details fadeIn-top">
         <h3>Simple music search app </h3>
-        <ul className="truck-overlay-text"style={{listStyleType: 'none', color: 'white'}}>
-          <li>Javascript</li>
-          <li>iTunes API</li>
-          <li>CSS</li>
-        </ul>
+          <ul className="truck-overlay-text"style={{listStyleType: 'none', color: 'white'}}>
+            <li>Javascript</li>
+            <li>iTunes API</li>
+            <li>CSS</li>
+          </ul>
       </div>
   </div>
   </a>
@@ -80,16 +76,16 @@ export default class Input extends Component {
 </div>
 
 <div class="container">
-  <h3 class="title">Cat Sitting App (Prototype)</h3>
-  <a target="_blank" rel="noopener noreferrer" href="https://invis.io/V5NPNNL2FPM">
+  <h3 class="title">Clothing Store</h3>
+  <a target="_blank" rel="noopener noreferrer" href="https://node-react-fun.herokuapp.com/">
   <div class="content">
       <div class="content-overlay"></div>
-      <img class="content-image" alt="trucking-site" src="wireframing.png"/>
+      <img class="content-image" alt="shopping-site" src="shopping-site.png"/>
       <div class="content-details fadeIn-top">
-        <h3>Designed wireframes & prototyped an app for finding and scheduling with pet sitters.</h3>
+        <h3>Mocked up a website design for a clothing store.</h3>
         <ul className="truck-overlay-text"style={{listStyleType: 'none', color: 'white'}}>
-          <li>Sketch</li>
-          <li>Invision</li>
+          <li>React</li>
+          <li>Node.js</li>
         </ul>
       </div>
   </div>
@@ -97,7 +93,7 @@ export default class Input extends Component {
 </div>
 </div>
 <BottomNav/>
-<div className="design-credit"> <a target="_blank" rel="noopener noreferrer" href="https://www.behance.net/secludedenigma">Illustration design by Blake Walker</a></div>
+<div className="design-credit"> <a target="_blank" rel="noopener noreferrer" href="linkedin.com/in/blakewdesigns">Illustration by Blake Walker</a></div>
       </div>
     )
   }
